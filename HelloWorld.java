e66328c520ebb1efe31cac2de64a33156ef83bd0
Martin Kosek <kosek.martin@gmail.com>
2018-11-03 15:46:06 +0100

    5 commit

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
 
 	public static void main(String[] args){
 		for (int i=0; i<250; i++){
-		  System.out.println("Jenkins RULES - Hello world..");	
+		  System.out.println("Jenkins RULES - Hello world!");	
 		}	
 	}
 }
